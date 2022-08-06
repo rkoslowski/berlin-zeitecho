a17c485b39c6ca9f6a18f45b014405cdab4a8a09
unknown <rouvenkoslowski@icloud.com>
2022-08-07 01:52:54 +0200

    add: first selenium interaction /refactor: files and helper new organized/ code more efficiant

--- src/berlin_auto_appointment/buergerbot.py
+++ src/berlin_auto_appointment/buergerbot.py
@@ -3,25 +3,20 @@ import time
 import requests
 import helper_functions as hf
 
-url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[' \
-      ']=120703&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122238,122243,' \
-      '327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,' \
-      '122280,327294,122282,327290,122284,327292,122291,327270,122285,327266,122286,327264,122296,327268,150230,' \
-      '329760,122301,327282,122297,327286,122294,327284,122312,329763,122304,327330,122311,327334,122309,327332,' \
-      '122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,327653,' \
-      '122257,330265,327322,122208,327298,122226,' \
-      '327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120703%2F '
-
 def searchavailableappointments():
+    url = hf.get_appointment_link()
     while True:
+        # verify url integrity and connection
         hf.check_url(url)
 
-        response = requests.get(url)
+        # capture url request-response
+        response = hf.get_response(url)
 
-        hf.get_available_appointment_links_this_month(response)
-        hf.get_available_appointment_links_next_month(response)
-        # wait a random time between 90 to 120 sec to prevent the webserver to refuse connection (for ddos)
-        time.sleep(hf.randomize_time())
+        # execute the main code to retrieve appointment-links
+        hf.try_get_app(response)
+        
+        # prevent serverblocking by waiting a random period of time
+        hf.sleep_til_next_request()
 
 if __name__ == '__main__':
     searchavailableappointments()
--- src/berlin_auto_appointment/helper_functions.py
+++ src/berlin_auto_appointment/helper_functions.py
@@ -46,12 +46,64 @@ def get_available_appointment_links_next_month(response):
         for link in links[:10]:
             day = int(link.text.strip())
             found = True
-            print('{}: http://service.berlin.de{} '.format(day, link.attrs['href']))
+            #print('{}: http://service.berlin.de{} '.format(day, link.attrs['href']))
             ext_link = "http://service.berlin.de" + link.attrs['href']
             print(ext_link)
             if found:
                 open_in_browser(ext_link)
                 exit()
 
+
+# opens the target link in the browser
 def open_in_browser(link):
     wb.open_new_tab(link)
+
+# defines the type of service to check for available appointments
+# the booleans are to be set as the only input parameter
+# i really didnt want to make an gui...
+def get_servic_type():
+    anmeldung_einer_wohnung     = False
+    personal_id_beantragen      = True
+    reisepass_beantragen        = False
+    if(anmeldung_einer_wohnung):
+        return 'https://service.berlin.de/dienstleistung/120686/'
+    elif (personal_id_beantragen):
+        return 'https://service.berlin.de/dienstleistung/120703/'
+    else:
+        return 'https://service.berlin.de/dienstleistung/121151/'
+
+def get_appointment_link():
+    servicelink = get_servic_type()
+
+    try:
+        response = requests.get(servicelink)
+        response.raise_for_status()
+    except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+            print('Connection successfull!')
+
+    body = bs4.BeautifulSoup(response.content, 'html.parser')
+
+    if(body.find('div', class_='zmstermin-multi inner')):
+
+        bodyclass = body.find('a', class_='btn')
+        link = bodyclass.attrs['href']
+        return link
+    else:
+        print('required element not found')
+        exit()
+
+# wait a random time between 90 to 120 sec to prevent the webserver to refuse connection (for ddos)
+def sleep_til_next_request():
+    time.sleep(randomize_time())
+
+
+def get_response(url):
+    return requests.get(url)
+
+def try_get_app(urlbody):
+    get_available_appointment_links_this_month(urlbody)
+    get_available_appointment_links_next_month(urlbody)
