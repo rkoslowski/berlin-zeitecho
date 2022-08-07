07e08cdebd9d4651bc2d61ce1cff030948acd821
unknown <rouvenkoslowski@icloud.com>
2022-08-07 21:22:32 +0200

    add: critical code now light protected with exceptions

--- src/berlin_auto_appointment/buergerbot.py
+++ src/berlin_auto_appointment/buergerbot.py
@@ -1,22 +1,36 @@
-# import subprocess
-import time
-import requests
+from msilib.schema import Error
 import helper_functions as hf
 
 def searchavailableappointments():
-    url = hf.get_appointment_link()
+    # long link (including all days of a service)
+    url = hf.get_long_link()
     while True:
         # verify url integrity and connection
         hf.check_url(url)
 
-        # capture url request-response
-        response = hf.get_response(url)
-
         # execute the main code to retrieve appointment-links
-        hf.try_get_app(response)
+        try:
+            day = hf.try_get_app(url)
+            print(day)
+            if (day < 0 or day > 31):
+                raise ValueError
+        except TypeError :
+            print("variable day is of type ",type(day))
+            print('expected int32')
+            exit()
+        except ValueError :
+            print('variable not in boundry 1-31')
+            exit()
         
-        # prevent serverblocking by waiting a random period of time
-        hf.sleep_til_next_request()
+        if(day != 0):
+            print('emu start')
+            hf.browser_emulation(day)
+        elif(day == 0):
+            print('gonna wait')
+            # prevent serverblocking by waiting a random period of time
+            hf.sleep_til_next_request()
+        else:
+            print("uncaught exception here")
 
 if __name__ == '__main__':
     searchavailableappointments()
